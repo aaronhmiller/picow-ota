fe326937cc0f7ecb47299cd02e83d592a3adb55e
Aaron Miller <aaron.miller@chainguard.dev>
2025-12-02 11:23:22 -0800

    tidying

--- ota_updater.py
+++
@@ -1,7 +0,0 @@
-from ota import OTAUpdater
-from WIFI_CONFIG import SSID, PASSWORD
-
-firmware_url = "https://raw.githubusercontent.com/aaronhmiller/picow-ota/"
-
-ota_updater = OTAUpdater(SSID, PASSWORD, firmware_url, "main.py")
-ota_updater.download_and_install_update_if_available()
