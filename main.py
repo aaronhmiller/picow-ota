7dd062561556a2d380579f0e08dc6858e0f588ca
Aaron Miller <aaronhmiller@Family-Mac-mini.local>
2025-12-01 19:56:51 -0800

    fixing

--- main.py
+++ main.py
@@ -1,13 +1,5 @@
 from machine import Pin, Timer
-from ota import OTAUpdater
-from WIFI_CONFIG import SSID, PASSWORD
-
-firmware_url = "https://raw.githubusercontent.com/aaronhmiller/picow-ota/main"
-
-ota_updater = OTAUpdater(SSID, PASSWORD, firmware_url, "main.py")
-ota_updater.download_and_install_update_if_available()
-
-led = Pin(16, Pin.OUT)
+led = machine.Pin("LED", machine.Pin.OUT)
 
 def tick(timer):
     led.toggle()
