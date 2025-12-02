dfa8c798f70667347113ccbb155d6cbd5709f781
Aaron Miller <aaronhmiller@users.noreply.github.com>
2025-12-01 19:53:43 -0800

    Change OTAUpdater script name from 'ota_updater.py' to 'main.py'

--- ota_updater.py
+++ ota_updater.py
@@ -3,5 +3,5 @@ from WIFI_CONFIG import SSID, PASSWORD
 
 firmware_url = "https://raw.githubusercontent.com/aaronhmiller/picow-ota/"
 
-ota_updater = OTAUpdater(SSID, PASSWORD, firmware_url, "ota_updater.py")
+ota_updater = OTAUpdater(SSID, PASSWORD, firmware_url, "main.py")
 ota_updater.download_and_install_update_if_available()
