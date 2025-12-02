58edaa5e003f3f53d1776b1587af17f645284b30
Aaron Miller <aaronhmiller@users.noreply.github.com>
2025-12-01 20:05:14 -0800

    Add OTAUpdater for firmware updates
    Integrate OTA update functionality for firmware.

--- main.py
+++ main.py
@@ -1,3 +1,9 @@
+from ota import OTAUpdater
+from WIFI_CONFIG import SSID, PASSWORD
+firmware_url = "https://raw.githubusercontent.com/aaronhmiller/picow-ota/"
+ota_updater = OTAUpdater(SSID, PASSWORD, firmware_url, "main.py")
+ota_updater.download_and_install_update_if_available()
+
 from machine import Pin, Timer
 led = machine.Pin("LED", machine.Pin.OUT)
 
